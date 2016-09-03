59c6c1d84d94420e76ba860d5b5218fc0c0d69dd
Cocoa <cocoa@kiwki.us>
2016-09-03 20:46:56 +0900

    좋아 커밋이다

---
+++ Hoto.php
@@ -0,0 +1,40 @@
+<?php
+
+if ( ! defined( 'MEDIAWIKI' ) ) die( "This is an extension to the MediaWiki package and cannot be run standalone." );
+
+$wgExtensionCredits['skin'][] = array(
+	'path'        => __FILE__,
+	'name'        => 'Skin',
+	'url'         => 'https://shapebootstrap.net/item/1524925-acme-free-responsive-corporate-template/comments',
+	'author'      => 'cosmic, 김동동, 2DU, 코코아',
+	'description' => 'HotoWiki의 기본테마입니다.',
+        'license-name' => "FREE LICENSE",
+        'version' => 'Release',
+);
+
+$wgValidSkinNames['skin'] = 'Skin';
+$wgAutoloadClasses['SkinHoto'] = __DIR__ . '/Skin.php';
+
+
+$skinDirParts = explode( DIRECTORY_SEPARATOR, __DIR__ );
+$skinDir = array_pop( $skinDirParts );
+
+$wgResourceModules['skins.hoto'] = array(
+	'styles' => array(
+		$skinDir . '/css/bootstrap.min.css'            => array( 'media' => 'all' ),
+		$skinDir . '/css/theme.css'            => array( 'media' => 'all' ),
+		$skinDir . '/css/bootstrap-reset.css'            => array( 'media' => 'all' ),
+		$skinDir . '/css/style.css'            => array( 'media' => 'all' ),
+		$skinDir . '/css/style-responsive.css'            => array( 'media' => 'all' ),
+		$skinDir . '/css/librewiki.css'            => array( 'media' => 'all' ),
+	),
+	'scripts' => array(
+		$skinDir . '/js/jquery.min.js',
+		$skinDir . '/js/bootstrap.min.js',
+		$skinDir . '/js/adsbygoogle.js',
+		$skinDir . '/js/Gadget-ReferenceTooltips-ko.js',
+	),
+	'remoteBasePath' => &$GLOBALS['wgStylePath'],
+	'localBasePath'  => &$GLOBALS['wgStyleDirectory'],
+);
+ 
---
+++ README.md
@@ -0,0 +1,4 @@
+# Kiwitic
+키위위키 기본 스킨(Thanks for https://wsilog.xyz 2DU)
+
+원본 : https://github.com/2DU/acme
